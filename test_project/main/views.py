982ef2ea20087fb3c75a78e3c4bd334434b93c01
Elnur <bonvivan1982@gmail.com>
2023-08-22 11:51:31 +0300

    button

--- test_project/main/views.py
+++ test_project/main/views.py
@@ -5,15 +5,16 @@ from django.shortcuts import render
 def index(request):
     data = {
         'title': 'Главная страница',
-        #'values': [True, 'Hello', 123],
-        #'obj': {
-            #'car': 'BMW',
-            #'age': 18,
-            #'hobby': 'Football'
+        # 'values': [True, 'Hello', 123],
+        # 'obj': {
+        # 'car': 'BMW',
+        # 'age': 18,
+        # 'hobby': 'Football'
 
-        #}
+        # }
     }
-    return render(request, 'main/index.html',data)
+    return render(request, 'main/index.html', data)
+
 
 def about(request):
     return render(request, 'main/about.html')
--- test_project/news/views.py
+++ test_project/news/views.py
@@ -1,11 +1,22 @@
 from django.shortcuts import render, redirect
 from .models import Articles
 from .forms import ArticlesForm
+from django.views.generic import DetailView
 
 def news_home(request):
     #news = Articles.objects.all()
     news = Articles.objects.order_by('-date')
     return render(request,'news/news_home.html',{'news':news})
+
+
+def detail_view(request):
+    return render(request,'news/details_view.html', {'detail_view':detail_view})
+
+class NewsDetailView(DetailView):
+    model = Articles
+    template_name = 'news/details_view.html'
+    context_object_name = 'article'
+
 
 def create(request):
     error = ''
